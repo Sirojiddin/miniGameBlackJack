c6c8d87d8ca1b30ab8a47bb832424fdf67df6780
Sirojiddin Khamidov <sirojiddin89@gmail.com>
2022-08-25 18:24:05 +0500

    deleted old code shape

--- js.js
+++ js.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// let colorPickArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
-
 document.addEventListener("click", function () {
   // Card Suit and values
   let cardValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
