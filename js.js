591b827b169ffdebe1b6777a26bc2f8a32686724
Sirojiddin Khamidov <sirojiddin89@gmail.com>
2022-08-25 18:21:22 +0500

    gives some result but not completed yet

--- js.js
+++ js.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// let colorPickArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
+document.addEventListener("click", function () {
+  // Card Suit and values
+  let cardValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let cardSuit = ["♠️", "♣️", "♥️", "♦️"];
+  for (let i = 0; i < 5; i++) {
+    let selectedCard = [];
+    let valueRandomizer = Math.floor(Math.random() * cardValue.length);
+    let suitRandomizer = Math.floor(Math.random() * cardSuit.length);
+    selectedCard.unshift(cardValue[valueRandomizer], cardSuit[suitRandomizer]);
+    document.querySelector(`#card${i + 1}`).textContent = selectedCard.join("");
+    console.log(selectedCard);
+  }
+
+  //   let selectedColor = [];
+  //   for (let i = 0; i < 6; i++) {
+  //     let randomizer = Math.floor(Math.random() * 16);
+  //     selectedColor.unshift(colorPickArr[randomizer]);
+  //   }
+  //   let colorName = "#" + selectedColor.join("");
+
+  //   document.querySelector("body").style.backgroundColor = colorName;
+});
